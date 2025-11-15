8bdc5ef12381564d2590dd5e1dc5f4e32f266000
Pritom Dutta <pritom00019@gmail.com>
2025-11-16 02:08:51 +0600

    Add branch restriction

--- core/domain/src/main/java/com/pritom/domain/repository/MovieRepository.kt
+++ core/domain/src/main/java/com/pritom/domain/repository/MovieRepository.kt
@@ -4,6 +4,7 @@ import com.pritom.domain.model.Movie
 import com.pritom.domain.model.MovieCategory
 import kotlinx.coroutines.flow.Flow
 
+
 interface MovieRepository {
     suspend fun getMovies(category: MovieCategory): Flow<List<Movie>>
 }
