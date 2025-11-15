1e5199dad66afbd8905563b91058e0612fdb9b8b
Pritom Dutta <pritom00019@gmail.com>
2025-11-16 02:14:09 +0600

    add test class for domain getmovieusecase class

---
+++ core/domain/src/test/java/com/pritom/domain/GetMoviesUseCaseTest.kt
@@ -0,0 +1,15 @@
+package com.pritom.domain
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class GetMoviesUseCaseTest {
+
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
